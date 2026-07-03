a8f7b669b46ec355d2e5982449ac1f4ae8f7d90a
muuhesham <muhammedhesham488@gmail.com>
2026-07-03 10:25:50 +0300

    fix(global handler error): detect bug in handler error

--- backend/src/middleware/global-error-handler.js
+++ backend/src/middleware/global-error-handler.js
@@ -1,6 +1,6 @@
 export function errorHandler(error, _request, response, _next) {
   const statusCode = error.statusCode || 500;
-  const message = statusCode.startsWith('4') ? error.message : 'Internal server error';
+  const message = (statusCode >= 400 && statusCode < 500) ? error.message : 'Internal server error';
 
   response.status(statusCode).json({ message });
 }
